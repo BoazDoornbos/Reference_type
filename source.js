eb5728582561e06a48eb2ba6125b57ea69ec398b
Boaz Doornbos <84346524+BoazDoornbos@users.noreply.github.com>
2021-07-13 20:50:36 +0200

    Initial commit

---
+++ source.js
@@ -0,0 +1,15 @@
+let person = {
+    name: 'boaz',
+    age: 20,
+};
+let evaluations = [
+    7,
+    10,
+    9
+]
+
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person['name']);
+// console.log(person['age']);
+console.log(evaluations);
